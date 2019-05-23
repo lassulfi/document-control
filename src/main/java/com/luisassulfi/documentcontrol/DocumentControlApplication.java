3cb6078cf5be9a5a726baec29b05fed907d99f59
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-05-22 22:43:41 -0300

    First commit - Project started

---
+++ src/main/java/com/luisassulfi/documentcontrol/DocumentControlApplication.java
@@ -0,0 +1,13 @@
+package com.luisassulfi.documentcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentControlApplication.class, args);
+	}
+
+}
